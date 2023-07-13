f73627ae9c2b3a170dc121fc01bae40ba0813e46
Ferdinand Kapl <f.kapl@web.de>
2023-07-13 16:47:04 +0200

    Fixed small errors, cleaned notebook and added requirements for packages

--- general_models.py
+++ general_models.py
@@ -135,7 +135,7 @@ def log_normal(x, mu=None, v=None, reduce=True):
     if mu.shape[1] != v.shape[0] and mu.shape != v.shape:
         raise ValueError(f'The mean and variance vector do not have the same shape:\n\tmean: {mu.shape}\tvariance: {v.shape}')
 
-    logpdf = -0.5 * (torch.log(2 * np.pi) + v.log() + (x - mu).pow(2).div(v))
+    logpdf = -0.5 * (np.log(2 * np.pi) + v.log() + (x - mu).pow(2).div(v))
 
     if reduce:
         logpdf = logpdf.sum(dim=-1)
